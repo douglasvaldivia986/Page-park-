adbfabc9605e9eebfb92e2ec984343dad6742262
douglasvaldivia986 <107345751+douglasvaldivia986@users.noreply.github.com>
2023-01-05 02:06:39 -0400

    0012

---
+++ botones.js
@@ -0,0 +1,9 @@
+function mostrar() {
+    document.getElementById('articles').style.display = 'block';
+    document.getElementById('cont_ver').style.display = 'none';
+}
+
+function ocultar() {
+    document.getElementById('articles').style.display = 'none';
+    document.getElementById('cont_ver').style.display = 'block';
+}
