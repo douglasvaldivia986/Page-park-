9383186e8b163a8e6db86545ab3ceb83a7a34018
douglasvaldivia986 <107345751+douglasvaldivia986@users.noreply.github.com>
2023-01-12 01:57:46 -0400

    Add files via upload

--- botones.js
+++ botones.js
@@ -7,3 +7,13 @@ function ocultar() {
     document.getElementById('articles').style.display = 'none';
     document.getElementById('cont_ver').style.display = 'block';
 }
+
+window.onscroll = function() {
+    if (window.scrollY > document.getElementById('cartel').offsetTop) {
+        document.getElementById('cartel').style.display = "none";
+        document.getElementById('margin-cartel').style.margin = '220px 0px 0px 0px';
+    } else {
+        document.getElementById('cartel').style.display = "block";
+        document.getElementById('margin-cartel').style.margin = '300px 0px 0px 0px';
+    }
+  };
