b6143151d73aa88ef64d9029d8bf44d5c9ba9e4b
Zpdoz0 <111463615+Zpdoz0@users.noreply.github.com>
2023-01-16 20:01:24 -0400

    Add files via upload

--- botones.js
+++ botones.js
@@ -8,6 +8,15 @@ function ocultar() {
     document.getElementById('cont_ver').style.display = 'block';
 }
 
+/*cambiar el id imitando una media queris
+/*window.addEventListener("resize", function() {
+    if (window.innerWidth < 1256) {
+      document.getElementById("cartel").setAttribute("id", "responsive");
+    } else {
+      document.getElementById("responsive").setAttribute("id", "cartel");
+    }
+  });*/
+
 window.onscroll = function() {
     if (window.scrollY > document.getElementById('cartel').offsetTop) {
         document.getElementById('cartel').style.display = "none";
@@ -17,3 +26,20 @@ window.onscroll = function() {
         document.getElementById('margin-cartel').style.margin = '300px 0px 0px 0px';
     }
   };
+
+/*funciones responsive */
+
+function mostraresp() {
+    document.getElementById('articles-resp').style.display = 'block';
+    document.getElementById('cont_veresp').style.display = 'none';
+}
+
+function ocultaresp() {
+    document.getElementById('articles-resp').style.display = 'none';
+    document.getElementById('cont_veresp').style.display = 'block';
+}
+  
+  
+  
+  
+  
