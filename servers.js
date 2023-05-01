76b88e94aa16a1ed6b44498e26ae3a4403cd487b
douglasvaldivia986 <107345751+douglasvaldivia986@users.noreply.github.com>
2023-05-01 15:19:35 -0400

    Add files via upload
    0.0.01

--- servers.js
+++ servers.js
@@ -1,50 +1,79 @@
-function sb() {
-    document.getElementById('sb').style.display = 'block'
-    document.getElementById('send').style.display = 'none'
-    document.getElementById('drive').style.display = 'none'
-    document.getElementById('shar').style.display = 'none'
-    var send = document.getElementById('send');
-    send.src = send.src;
-    var drive = document.getElementById('drive');
-    drive.src = drive.src;
-    var shar = document.getElementById('shar');
-    shar.src = shar.src;
-}
-
-function send() {
-    document.getElementById('send').style.display = 'block'
-    document.getElementById('sb').style.display = 'none'
-    document.getElementById('drive').style.display = 'none'
-    document.getElementById('shar').style.display = 'none'
-    var sb = document.getElementById('sb');
-    sb.src = sb.src;
-    var drive = document.getElementById('drive');
-    drive.src = drive.src;
-    var shar = document.getElementById('shar');
-    shar.src = shar.src;
-}
-
 function drive() {
     document.getElementById('drive').style.display = 'block'
+    document.getElementById('lare').style.display = 'none'
     document.getElementById('sb').style.display = 'none'
-    document.getElementById('send').style.display = 'none'
-    document.getElementById('shar').style.display = 'none'
+    document.getElementById('tape').style.display = 'none'
+    document.getElementById('mega').style.display = 'none'
     var sb = document.getElementById('sb');
     sb.src = sb.src;
-    var send = document.getElementById('send');
+    var send = document.getElementById('lare');
     send.src = send.src;
-    var shar = document.getElementById('shar');
+    var shar = document.getElementById('tape');
     shar.src = shar.src;
+    var send = document.getElementById('mega');
+    send.src = send.src;
 }
-function shar() {
-    document.getElementById('shar').style.display = 'block'
-    document.getElementById('sb').style.display = 'none'
+
+function lare() {
+    document.getElementById('lare').style.display = 'block'
     document.getElementById('drive').style.display = 'none'
-    document.getElementById('send').style.display = 'none'
-    var sb = document.getElementById('sb');
+    document.getElementById('sb').style.display = 'none'
+    document.getElementById('tape').style.display = 'none'
+    document.getElementById('mega').style.display = 'none'
+    var sb = document.getElementById('drive');
     sb.src = sb.src;
-    var drive = document.getElementById('drive');
+    var drive = document.getElementById('sb');
     drive.src = drive.src;
-    var send = document.getElementById('send');
+    var shar = document.getElementById('tape');
+    shar.src = shar.src;
+    var send = document.getElementById('mega');
+    send.src = send.src;
+}
+
+function sb() {
+    document.getElementById('sb').style.display = 'block'
+    document.getElementById('drive').style.display = 'none'
+    document.getElementById('lare').style.display = 'none'
+    document.getElementById('tape').style.display = 'none'
+    document.getElementById('mega').style.display = 'none'
+    var send = document.getElementById('drive');
+    send.src = send.src;
+    var drive = document.getElementById('lare');
+    drive.src = drive.src;
+    var shar = document.getElementById('tape');
+    shar.src = shar.src;
+    var send = document.getElementById('mega');
+    send.src = send.src;
+}
+
+function tape() {
+    document.getElementById('tape').style.display = 'block'
+    document.getElementById('drive').style.display = 'none'
+    document.getElementById('lare').style.display = 'none'
+    document.getElementById('sb').style.display = 'none'
+    document.getElementById('mega').style.display = 'none'
+    var sb = document.getElementById('drive');
+    sb.src = sb.src;
+    var drive = document.getElementById('lare');
+    drive.src = drive.src;
+    var send = document.getElementById('sb');
+    send.src = send.src;
+    var send = document.getElementById('mega');
+    send.src = send.src;
+}
+
+function mega() {
+    document.getElementById('mega').style.display = 'block'
+    document.getElementById('drive').style.display = 'none'
+    document.getElementById('lare').style.display = 'none'
+    document.getElementById('sb').style.display = 'none'
+    document.getElementById('tape').style.display = 'none'
+    var sb = document.getElementById('drive');
+    sb.src = sb.src;
+    var drive = document.getElementById('lare');
+    drive.src = drive.src;
+    var send = document.getElementById('sb');
+    send.src = send.src;
+    var send = document.getElementById('tape');
     send.src = send.src;
 }
